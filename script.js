336778419bb6923d134b652d1aa86b5f180d3355
ResinUa <sonyechkozoti@gmail.com>
2026-05-15 16:23:43 +0200

    I did it

--- script.js
+++ script.js
@@ -1,91 +1,240 @@
 //константи (kill me)
-const goleft = document.getElementById('left');
-const goright = document.getElementById('right'); 
-const count = document.getElementById('count');
+const goleft = document.getElementById("left");
+const goright = document.getElementById("right");
+const count = document.getElementById("count");
 
-const sc2p1 = document.GetElementById('sc2p1'); //active group
-const sc2p2 = document.GetElementById('sc2p2');
-const sc2p3 = document.GetElementById('sc2p3');
-const sc2p4 = document.GetElementById('sc2p4');
-const sc2p5 = document.GetElementById('sc2p5');
-const sc2p6 = document.GetElementById('sc2p6');
-const sc2p7 = document.GetElementById('sc2p7');
-const sc2p8 = document.GetElementById('sc2p8');
-const sc2p9 = document.GetElementById('sc2p9');
-const sc2p10 = document.GetElementById('sc2p10');
-const sc2p11 = document.GetElementById('sc2p11');
-const sc2p12 = document.GetElementById('sc2p12');
-const sc2p13 = document.GetElementById('sc2p13');
-const sc2p14 = document.GetElementById('sc2p14');
-const sc2p15 = document.GetElementById('sc2p15');
-
-const sc2pc1 = document.GetElementById('sc2pc1'); //activething
-const sc2pc2 = document.GetElementById('sc2pc2');
-const sc2pc3 = document.GetElementById('sc2pc3');
-const sc2pc4 = document.GetElementById('sc2pc4');
-const sc2pc5 = document.GetElementById('sc2pc5');
-const sc2pc6 = document.GetElementById('sc2pc6');
-const sc2pc7 = document.GetElementById('sc2pc7');
-const sc2pc8 = document.GetElementById('sc2pc8');
-const sc2pc9 = document.GetElementById('sc2pc9');
-const sc2pc10 = document.GetElementById('sc2pc10');
-const sc2pc11 = document.GetElementById('sc2pc11');
-const sc2pc12 = document.GetElementById('sc2pc12');
-const sc2pc13 = document.GetElementById('sc2pc13');
-const sc2pc14 = document.GetElementById('sc2pc14');
-const sc2pc15 = document.GetElementById('sc2pc15');
-
-const star1 = document.GetElementById('star1'); //activestar
-const star2 = document.GetElementById('star2');
-const star3 = document.GetElementById('star3');
-const star4 = document.GetElementById('star4');
-const star5 = document.GetElementById('star5');
-const star6 = document.GetElementById('star6');
-const star7 = document.GetElementById('star7');
-const star8 = document.GetElementById('star8');
-const star9 = document.GetElementById('star9');
-const star10 = document.GetElementById('star10');
-const star11 = document.GetElementById('star11');
-const star12 = document.GetElementById('star12');
-const star13 = document.GetElementById('star13');
-const star14 = document.GetElementById('star14');
-const star15 = document.GetElementById('star15');
-
-const text1 = document.GetElementById('text1'); //activetext
-const text2 = document.GetElementById('text2');
-const text3 = document.GetElementById('text3');
-const text4 = document.GetElementById('text4');
-const text5 = document.GetElementById('text5');
-const text6 = document.GetElementById('text6');
-const text7 = document.GetElementById('text7');
-const text8 = document.GetElementById('text8');
-const text9 = document.GetElementById('text9');
-const text10 = document.GetElementById('text10');
-const text11 = document.GetElementById('text11');
-const text12 = document.GetElementById('text12');
-const text13 = document.GetElementById('text13');
-const text14 = document.GetElementById('text14');
-const text15 = document.GetElementById('text15');
+const sc2p1 = document.getElementById("sc2p1");
+const sc2p2 = document.getElementById("sc2p2");
+const sc2p3 = document.getElementById("sc2p3");
+const sc2p4 = document.getElementById("sc2p4");
+const sc2p5 = document.getElementById("sc2p5");
+const sc2p6 = document.getElementById("sc2p6");
+const sc2p7 = document.getElementById("sc2p7");
+const sc2p8 = document.getElementById("sc2p8");
+const sc2p9 = document.getElementById("sc2p9");
+const sc2p10 = document.getElementById("sc2p10");
+const sc2p11 = document.getElementById("sc2p11");
+const sc2p12 = document.getElementById("sc2p12");
+const sc2p13 = document.getElementById("sc2p13");
+const sc2p14 = document.getElementById("sc2p14");
+const sc2p15 = document.getElementById("sc2p15");
 
 //слайдер
-let countthing = 2;
+var countthing = 2;
 
-goleft.addEventListener('click', () => {
-    if (countthing == 2) {
-     text1.cla
-    }
+goleft.addEventListener("click", () => {
+  if (countthing == 2) {
+    [sc2p1, sc2p2].forEach((el) => el.classList.toggle("middle")); //1
+    [sc2p15, sc2p1].forEach((el) => el.classList.toggle("adaptivefirst")); //15
+    [sc2p2, sc2p3].forEach((el) => el.classList.toggle("adaptivelast")); //2
+    countthing = 1;
+    console.log(countthing);
+    count.textContent = "1/15";
+  } else if (countthing == 1) {
+    [sc2p15, sc2p1].forEach((el) => el.classList.toggle("middle"));
+    [sc2p14, sc2p15].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p1, sc2p2].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 15;
+    count.textContent = "15/15";
+  } else if (countthing == 15) {
+    [sc2p14, sc2p15].forEach((el) => el.classList.toggle("middle"));
+    [sc2p13, sc2p14].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p15, sc2p1].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 14;
+    count.textContent = "14/15";
+  } else if (countthing == 14) {
+    [sc2p13, sc2p14].forEach((el) => el.classList.toggle("middle"));
+    [sc2p12, sc2p13].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p14, sc2p15].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 13;
+    count.textContent = "13/15";
+  } else if (countthing == 13) {
+    [sc2p12, sc2p13].forEach((el) => el.classList.toggle("middle"));
+    [sc2p11, sc2p12].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p13, sc2p14].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 12;
+    count.textContent = "12/15";
+  } else if (countthing == 12) {
+    [sc2p11, sc2p12].forEach((el) => el.classList.toggle("middle"));
+    [sc2p10, sc2p11].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p12, sc2p13].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 11;
+    count.textContent = "11/15";
+  } else if (countthing == 11) {
+    [sc2p10, sc2p11].forEach((el) => el.classList.toggle("middle"));
+    [sc2p9, sc2p10].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p11, sc2p12].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 10;
+    count.textContent = "10/15";
+  } else if (countthing == 10) {
+    [sc2p9, sc2p10].forEach((el) => el.classList.toggle("middle"));
+    [sc2p8, sc2p9].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p10, sc2p11].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 9;
+    count.textContent = "9/15";
+  } else if (countthing == 9) {
+    [sc2p8, sc2p9].forEach((el) => el.classList.toggle("middle"));
+    [sc2p7, sc2p8].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p9, sc2p10].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 8;
+    count.textContent = "8/15";
+  } else if (countthing == 8) {
+    [sc2p7, sc2p8].forEach((el) => el.classList.toggle("middle"));
+    [sc2p6, sc2p7].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p8, sc2p9].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 7;
+    count.textContent = "7/15";
+  } else if (countthing == 7) {
+    [sc2p6, sc2p7].forEach((el) => el.classList.toggle("middle"));
+    [sc2p5, sc2p6].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p7, sc2p8].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 6;
+    count.textContent = "6/15";
+  } else if (countthing == 6) {
+    [sc2p5, sc2p6].forEach((el) => el.classList.toggle("middle"));
+    [sc2p4, sc2p5].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p6, sc2p7].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 5;
+    count.textContent = "5/15";
+  } else if (countthing == 5) {
+    [sc2p4, sc2p5].forEach((el) => el.classList.toggle("middle"));
+    [sc2p3, sc2p4].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p5, sc2p6].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 4;
+    count.textContent = "4/15";
+  } else if (countthing == 4) {
+    [sc2p3, sc2p4].forEach((el) => el.classList.toggle("middle"));
+    [sc2p2, sc2p3].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p4, sc2p5].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 3;
+    count.textContent = "3/15";
+  } else if (countthing == 3) {
+    [sc2p2, sc2p3].forEach((el) => el.classList.toggle("middle"));
+    [sc2p1, sc2p2].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p3, sc2p4].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 2;
+    count.textContent = "2/15";
+  }
+});
+
+goright.addEventListener("click", () => {
+  if (countthing == 2) {
+    [sc2p3, sc2p2].forEach((el) => el.classList.toggle("middle"));
+    [sc2p2, sc2p1].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p4, sc2p3].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 3;
+    count.textContent = "3/15";
+  } else if (countthing == 3) {
+    [sc2p4, sc2p3].forEach((el) => el.classList.toggle("middle"));
+    [sc2p3, sc2p2].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p5, sc2p4].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 4;
+    count.textContent = "4/15";
+  } else if (countthing == 4) {
+    [sc2p5, sc2p4].forEach((el) => el.classList.toggle("middle"));
+    [sc2p4, sc2p3].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p6, sc2p5].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 5;
+    count.textContent = "5/15";
+  } else if (countthing == 5) {
+    [sc2p6, sc2p5].forEach((el) => el.classList.toggle("middle"));
+    [sc2p5, sc2p4].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p7, sc2p6].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 6;
+    count.textContent = "6/15";
+  } else if (countthing == 6) {
+    [sc2p7, sc2p6].forEach((el) => el.classList.toggle("middle"));
+    [sc2p6, sc2p5].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p8, sc2p7].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 7;
+    count.textContent = "7/15";
+  } else if (countthing == 7) {
+    [sc2p8, sc2p7].forEach((el) => el.classList.toggle("middle"));
+    [sc2p7, sc2p6].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p9, sc2p8].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 8;
+    count.textContent = "8/15";
+  } else if (countthing == 8) {
+    [sc2p9, sc2p8].forEach((el) => el.classList.toggle("middle"));
+    [sc2p8, sc2p7].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p10, sc2p9].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 9;
+    count.textContent = "9/15";
+  } else if (countthing == 9) {
+    [sc2p10, sc2p9].forEach((el) => el.classList.toggle("middle"));
+    [sc2p9, sc2p8].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p11, sc2p10].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 10;
+    count.textContent = "10/15";
+  } else if (countthing == 10) {
+    [sc2p11, sc2p10].forEach((el) => el.classList.toggle("middle"));
+    [sc2p10, sc2p9].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p12, sc2p11].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 11;
+    count.textContent = "11/15";
+  } else if (countthing == 11) {
+    [sc2p12, sc2p11].forEach((el) => el.classList.toggle("middle"));
+    [sc2p11, sc2p10].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p13, sc2p12].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 12;
+    count.textContent = "12/15";
+  } else if (countthing == 12) {
+    [sc2p13, sc2p12].forEach((el) => el.classList.toggle("middle"));
+    [sc2p12, sc2p11].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p14, sc2p13].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 13;
+    count.textContent = "13/15";
+  } else if (countthing == 13) {
+    [sc2p14, sc2p13].forEach((el) => el.classList.toggle("middle"));
+    [sc2p13, sc2p12].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p15, sc2p14].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 14;
+    count.textContent = "14/15";
+  } else if (countthing == 14) {
+    [sc2p15, sc2p14].forEach((el) => el.classList.toggle("middle"));
+    [sc2p14, sc2p13].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p1, sc2p15].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 15;
+    count.textContent = "15/15";
+  } else if (countthing == 15) {
+    [sc2p1, sc2p15].forEach((el) => el.classList.toggle("middle"));
+    [sc2p15, sc2p14].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p2, sc2p1].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 1;
+    count.textContent = "1/15";
+  } else if (countthing == 1) {
+    [sc2p2, sc2p1].forEach((el) => el.classList.toggle("middle"));
+    [sc2p1, sc2p15].forEach((el) => el.classList.toggle("adaptivefirst"));
+    [sc2p3, sc2p2].forEach((el) => el.classList.toggle("adaptivelast"));
+    countthing = 2;
+    count.textContent = "2/15";
+  }
 });
 
 //тема
-const cbody = document.getElementById('cbody');
+const cbody = document.getElementById("cbody");
 let themetoggle = false;
 
-document.getElementById('aboba').addEventListener('click', () => {
-    if (themetoggle == false) {
-        cbody.style.background = 'rgb(79, 77, 76)';
-        themetoggle = true;
-    } else {
-        cbody.style.background = 'linear-gradient(180.11deg, #E5E2E3 10.73%, #FDFEFE 38.02%, #E5E2E3 71.11%, #FDFEFE 99.91%)';
-        themetoggle = false;
-    }
+document.getElementById("aboba").addEventListener("click", () => {
+  if (themetoggle == false) {
+    cbody.style.background = "rgb(79, 77, 76)";
+    themetoggle = true;
+  } else {
+    cbody.style.background =
+      "linear-gradient(180.11deg, #E5E2E3 10.73%, #FDFEFE 38.02%, #E5E2E3 71.11%, #FDFEFE 99.91%)";
+    themetoggle = false;
+  }
+});
+
+//модалка
+
+ismodalopen = false;
+
+document.querySelectorAll('copenmodal').addEventListener('click', () => {
+  if (ismodalopen == false) {
+    document.getElementsByClassName('cmodalwrap').style.display = 'flex';
+    ismodalopen = true;
+  } else {
+    document.getElementsByClassName('cmodalwrap').style.display = 'none';
+  }
 });
